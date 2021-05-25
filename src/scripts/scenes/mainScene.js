597480c29bcb6adca1474cef55fc3f23a19c8cd7
Sutan Daffa Satria Hertanto <lagooo178@gmail.com>
2021-05-25 16:46:13 +0700

    fixes to play music button

--- src/scripts/scenes/mainScene.js
+++ src/scripts/scenes/mainScene.js
@@ -23,27 +23,19 @@ export default class MainScene extends Phaser.Scene {
     var bg = this.add.sprite(0,0,'background');
     bg.setOrigin(0,0);
     this.mute = this.add.image(40, 40, 'soundoff').setScale(0.8).setAlpha(0);
-    this.play = this.add.image(40, 40, 'soundon').setScale(0.8).setAlpha(0);
+    this.play = this.add.image(40, 40, 'soundon').setScale(0.8).setAlpha(1);
     var tittle1 = this.add.image(620,70, 'tittle');
     //var titlemenu = this.add.text(600,130, 'MAIN MENU', {fontFamily:'Anton'}, {fontSize:'1200px'});
     button1 = this.add.image(650,190, 'buttonBG').setInteractive();
-    button2 = this.add.image(650,260, 'buttonBH').setInteractive();
-    button3 = this.add.image(650,330, 'buttonBI').setInteractive();
-    button4 = this.add.image(650,400, 'buttonBJ').setInteractive();
-    button5 = this.add.image(900,650, 'soundon').setInteractive();
-    button6 = this.add.image(650,470, 'buttonBL').setInteractive();
+    button3 = this.add.image(650,260, 'buttonBI').setInteractive();
+    button4 = this.add.image(650,330, 'buttonBJ').setInteractive();
+    button6 = this.add.image(650,400, 'buttonBL').setInteractive();
     this.menuKeys = this.input.keyboard.addKeys('m');
 
 
     button1.on('pointerdown', function () {
 
         this.scene.start('GameScene');
-
-    }, this);
-
-    button2.on('pointerdown', function () {
-
-        this.scene.start('GameOver');
 
     }, this);
 
@@ -63,6 +55,7 @@ export default class MainScene extends Phaser.Scene {
   update() {
     if (Phaser.Input.Keyboard.JustDown(this.menuKeys.m)) {
       this.game.config.musicStatus = !this.game.config.musicStatus;
+      this.game.config.musicStatus ? this.play.setAlpha(1) : this.play.setAlpha(0);
       this.game.config.musicStatus ? this.mute.setAlpha(0) : this.mute.setAlpha(1);
       this.game.config.musicStatus ? this.soundtrack.play() : this.soundtrack.stop();
     }
